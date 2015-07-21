9aaa6ba57863feb6ae8120a40610592b0995c632
anthonybrown <tony@tonybrown.me>
2015-07-20 23:21:43 -0400

    add karma, phantomjs, mocha, chai tests

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp')
 var browsersync = require('browser-sync')
+var karma = require('karma').server
+
+
 
 gulp.task('serve', function () {
 	browsersync.init({
-		notify: false,	
-		port: 8888,	
+		notify: false,
+		port: 8888,
 		server: {
-			baseDir: ['app'],	
+			baseDir: ['app'],
 			routes: {
 				'/bower_components': 'bower_components'
 			}
@@ -15,6 +18,16 @@ gulp.task('serve', function () {
 
 	gulp.watch(['app/**/*.*'])
 		.on('change', browsersync.reload);
+})
+
+gulp.task('test-browser',function(done){
+	karma.start({
+		configFile: __dirname + '/karma.conf.js',
+		singleRun: true,
+		reporters:['mocha','coverage']
+	},function(){
+		done();
+	})
 })
 
 gulp.task('serve-test', function () {
